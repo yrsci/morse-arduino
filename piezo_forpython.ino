e761d702c005f9b075bdc4c870670a9041622d16
yrsci <31885317+yrsci@users.noreply.github.com>
2018-02-13 19:37:01 +0000

    Improved comments

--- piezo_forpython.ino
+++ piezo_forpython.ino
@@ -1,16 +1,20 @@
+// Initialise variables
 int sensorPin = 2;
 int buzzPin = 3;
 int freq = 500;
 
+// Setup code, run once at beginning
 void setup() {
-  // put your setup code here, to run once:
+  // Open serial port
   Serial.begin(9600);
+  // State which pin to read from & which to output to
   pinMode(sensorPin, INPUT);
   pinMode(buzzPin, OUTPUT);
 }
 
+// Main code, looped repeatedly
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read status of sensor; 1 = pressed, 0 = not pressed
   int sensorVal = digitalRead(sensorPin);
   if (sensorVal == 1) {
     tone(buzzPin, freq);  
